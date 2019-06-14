a88565a331bf09c62afce3863dd24eb68a668561
gizmore <gizmore@wechall.net>
2019-06-15 01:42:38 +0200

    6.09-r691_comment_edit

---
+++ GDT_User.php
@@ -0,0 +1,25 @@
+<?php
+namespace GDO\Profile;
+
+use GDO\UI\GDT_Link;
+
+/**
+ * Shows a profile link instead of username.
+ * 
+ * @author gizmore
+ * @since 6.09
+ */
+final class GDT_User extends \GDO\User\GDT_User
+{
+	public function getLink($name=null)
+	{
+		if ($user = $this->getUser())
+		{
+			return GDT_Link::make($name?$name:$this->name)->
+				rawLabel($user->displayNameLabel())->
+				href(href('Profile', 'View', "&id={$user->getID()}"));
+		}
+	}
+
+	public function renderCell() { return $this->getLink()->renderCell(); }
+}
