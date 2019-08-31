45faab95ef5c49762b115e2bc2e04372c696effb
gizmore <gizmore@wechall.net>
2019-08-31 16:28:58 +0200

    6.10-r42_german_lang

---
+++ lang/profile_de.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+'card_title_profile' => '%s\'s Profil',
+'card_subtitle_profile' => 'Mitglied seit %s',
+'profile_icq' => 'ICQ Nummer',
+'profile_phone' => 'Telefonnummer',
+'profile_wapp' => 'Whatsapp Nummer',
+'profile_website' => 'Websitee',
+'profile_about' => 'Über Sie',
+### Config
+'cfg_profile_visible' => 'Wer darf Ihr Profil sehen?',
+'cfg_profile_views' => 'Anzahl an Profilaufrufen',
+'cfg_profile_view' => 'Ihr Profil ansehen',
+### Sexo
+'sexual_orientation' => 'Sexuelle Ausrichtung',
+'enum_men' => 'Männer',
+'enum_women' => 'Frauen',
+'enum_both' => 'Beide',
+);
+
