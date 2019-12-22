137591ba1df8e95ba79000960903f2ebc6f2e7cc
gizmore <gizmore@wechall.net>
2019-12-22 18:59:18 +0100

    6.10-r1042_it

---
+++ lang/profile_it.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+'card_title_profile' => '%s\'s Profilo',
+'card_subtitle_profile' => 'Membro dal %s',
+'profile_icq' => 'ICQ Numero',
+'profile_phone' => 'Numero di telefono',
+'profile_wapp' => 'Numero Whatsapp',
+'profile_website' => 'Sito Internet',
+'profile_about' => 'Qualcosa di Lei',
+### Config
+'cfg_profile_visible' => 'Chi puó vedere il vostro profilo?',
+'cfg_profile_views' => 'Numero visualizazioni del vostro profilo',
+'cfg_profile_view' => 'Visualizzare profilo',
+### Sexo
+'sexual_orientation' => 'Direzionamento sessuale',
+'enum_men' => 'Uomo',
+'enum_women' => 'Donna',
+'enum_both' => 'tutti due',
+);
+
